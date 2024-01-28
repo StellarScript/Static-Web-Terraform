df8f24315556e71091663f4cc2bda241e17ec654
StellarScript <ej92@protonmail.com>
2024-01-27 19:57:04 -0800

    nextjs build asset prefix asset prfix

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -5,6 +5,7 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  assetPrefix: './',
   nx: {
     svgr: false,
   },
